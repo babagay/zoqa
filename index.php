4533a990882fedfae951f31cf19ae5977cc4f5c2
babagay <wonderer@i.ua>
2014-02-12 18:43:22 +0200

    a

--- index.php
+++ index.php
@@ -3,4 +3,12 @@ phpinfo(); // test
 ?>
 <h2>test h2</h2>
 <div></div>
-<div>test</div>
+<div>test</div>git clone git@github.org:babagay/zoqa.git
+https://github.com/babagay/zoqa
+
+Permission denied (publickey).
+fatal: Could not read from remote repository.
+
+git clone Please make sure you have the correct access rights and the repository exists.
+
+rsa-key-20140212
